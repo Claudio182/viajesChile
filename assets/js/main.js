56648dcb5518c0d86fb45854e0bba948147995ef
denon182 <claudio.aravenacontreras@gmail.com>
2024-03-28 10:53:26 -0300

    Last styles modifications, among others

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
-// Enable Tooltip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+  // Enable Tooltip ---------
+    const tooltipTriggerList = $('[data-bs-toggle="tooltip"]');// Select elements with data-bs-toggle="tooltip" attribute
+    tooltipTriggerList.each(function() { // Initialize tooltips on each element
+      $(this).tooltip();
+    });
+
+  // Smooth Scroll ----------
+  $('.smooth-scroll').on("click", function (event) {  //By clicking on an element with the Smooth-Scroll class
+    event.preventDefault(); //Prevents the default waterfall of the event
+    var hash = this.hash; // I assign the value of the href attribute that comes in This.hash to the varied hash
+    $("html, body").animate( // Animate start
+      {
+        scrollTop: $(hash).offset().top, // This determines the final displacement position.
+      },
+      300, //Specifies the duration of animation
+      function () { 
+        window.location.hash = hash; //Update the URL hash to match the section to which it has moved
+      }
+    );
+  });
+});
